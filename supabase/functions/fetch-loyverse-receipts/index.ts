fa356b6ffbdd74a20961bacda8772099f5b37e47
180degreecafe <180.degre.bh@gmail.com>
2025-05-22 23:38:25 +0300

    Create index.ts
    init edge function

---
+++ supabase/functions/fetch-loyverse-receipts/index.ts
@@ -0,0 +1,20 @@
+import { serve } from "https://deno.land/std@0.192.0/http/server.ts"
+
+const ACCESS_TOKEN = "8BobfhtFqRbW_PxkVcDhVF5Qp1U";
+
+serve(async () => {
+  const response = await fetch("https://api.loyverse.com/v1.0/receipts?limit=50", {
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+      Accept: "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" }
+  });
+});
+
+init edge function
